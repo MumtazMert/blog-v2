25fd7c563c41e2bd13278e82b13f0c68d2b20e0c
Mumtaz Mert Tekdemir <mumtazmert@gmail.com>
2025-05-16 11:23:35 +0300

    Feat : Secon post

--- data/posts.ts
+++ data/posts.ts
@@ -27,6 +27,25 @@ export const posts: Post[] = [
       "Kısacası burası kodla yazılmış bir günlük olacak. Bugün ilk yazımı \"neden blog yazıyorum\" diye seçtim çünkü her geliştiricinin bir noktada kendine sorması gereken bir soru bu: \"Öğrendiklerimi sadece kendime mi saklayacağım, yoksa paylaşarak büyüyecek miyim?\""
     ],
     coverImage: "/placeholder.svg?height=400&width=800",
+  },
+  {
+    id: "post-2",
+    title: "Müzik uygulaması başlangıç",
+    date: "May 16, 2025",
+    readTime: 2,
+    excerpt: "",
+    content: [
+    "Okuduğu  bir çalışmada yeni müzikler keşfetmenin 30 lu yaşlardan sonra neredeyse durduğunu öğrenmiş bulunmaktayım ve bu beni rahatsız etti.",
+      "Uygulama kafamda şimdilik şu şekilde :",
+        "• Kullanıcı Google ile giriş yapar",
+        "• O günkü modunu belirlemek için çok sıkmayacak şekilde az miktarda soru ile karşılaşır",
+        "• Sorulara verdiği cevaplar sonucunda 5 adet müzik önerisi alır",
+        "• Önerilen müzikleri beğenip beğenmediğini belirtesi için thumbs up veya thumbs down verir",
+        "• Verilen değerlendirme uygulama da kayıt altına alınır ve sonraki öneriler buna göre bgelirlenir.",
+
+        "İleride değişiklik olacaktır mutlaka ancak uygulamayı mümkün olduğunca sade ve amaca yönelik tutmak amacım.Uygulama iskeletini V0 (Vercel'in yapay zekası) ile oluşturdum.Devam ettikçe güncelleme yapacağım"
+    ],
+    coverImage: "/placeholder.svg?height=400&width=800"
   }
 
 ]
